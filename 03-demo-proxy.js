0be0728f51c94ce5dac46f5599b1aee032328b85
asakao <anisorpchia@gmail.com>
2024-07-02 23:02:06 +0800

    封装响应式

--- 03-demo-proxy.js
+++ 03-demo-proxy.js
@@ -1,17 +1,35 @@
-let product = {
+let item = {
     price: 5,
     quantity: 2
 }
 
-let proxiedProduct = new Proxy(product, {
-    get(target, key, receiver) {
-        console.log('get was called with key = ' + key)
-        return Reflect.get(target, key, receiver)
-    },
-    set(target, key, value, receiver) {
-        console.log('set was called with key = ' + key + ' and value = ' + value)
-        return Reflect.set(target, key, value, receiver)
+// let proxiedProduct = new Proxy(product, {
+//     get(target, key, receiver) {
+//         console.log('get was called with key = ' + key)
+//         return Reflect.get(target, key, receiver)
+//     },
+//     set(target, key, value, receiver) {
+//         console.log('set was called with key = ' + key + ' and value = ' + value)
+//         return Reflect.set(target, key, value, receiver)
+//     }
+// })
+// proxiedProduct.quantity = 5
+// console.log(proxiedProduct.quantity)
+
+function reactive(target){
+    const handler = {
+        get(target, key, receiver) {
+            console.log('get was called with key = ' + key)
+            return Reflect.get(target, key, receiver)
+        },
+        set(target, key, value, receiver) {
+            console.log('set was called with key = ' + key + ' and value = ' + value)
+            return Reflect.set(target, key, value, receiver)
+        }
     }
-})
-proxiedProduct.quantity = 5
-console.log(proxiedProduct.quantity)
+    return new Proxy(target, handler)
+}
+
+let product = reactive(item)
+product.quantity = 7
+console.log(product.quantity)
