bbff6754b0ef2c18571ea45a51bbb8934cdf7f87
zdshen <zdshen@chinaums.com>
2024-07-03 17:03:10 +0800

    feat: dep

---
+++ 08-demo-dep.js
@@ -0,0 +1,55 @@
+class Dep {
+    constructor() {
+      this.subscribers = new Set();
+    }
+
+    depend() {
+      if (activeEffect) {
+        this.subscribers.add(activeEffect);
+      }
+    }
+
+    notify() {
+      this.subscribers.forEach((sub) => sub());
+    }
+  }
+
+  let activeEffect = null;
+
+  function watchEffect(effect) {
+    activeEffect = effect;
+    effect();
+    activeEffect = null;
+  }
+
+  function reactive(target) {
+    const depMap = new WeakMap();
+
+    const getDep = (target, key) => {
+      let deps = depMap.get(target);
+      if (!deps) {
+        deps = new Map();
+        depMap.set(target, deps);
+      }
+      let dep = deps.get(key);
+      if (!dep) {
+        dep = new Dep();
+        deps.set(key, dep);
+      }
+      return dep;
+    };
+
+    return new Proxy(target, {
+      get(target, property, receiver) {
+        const dep = getDep(target, property);
+        dep.depend();
+        return Reflect.get(target, property, receiver);
+      },
+      set(target, property, value, receiver) {
+        const dep = getDep(target, property);
+        const result = Reflect.set(target, property, value, receiver);
+        dep.notify();
+        return result;
+      },
+    });
+  }
